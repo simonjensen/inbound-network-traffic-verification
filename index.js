13d80b07cd5ead6dab84a7dd3852d14311061316
Simon Jensen <admin@simon-jensen.net>
2019-05-25 18:22:38 +0200

    App fixed

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const server = http.createServer(function(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  let date = new Date();
+  console.log(date.toISOString(), ip, req.method, req.url);
 
-  console.log(`[$(ip)] $(req.method) $(req.url)`);
-  res.end('OK');
+  res.end("OK\n");
 });
 
 server.listen(8080);
