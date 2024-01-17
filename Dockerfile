e349974620f42ee52c501549a8ee4463c9ec9eba
Simon Jensen <hello@simon-jensen.net>
2024-01-17 16:45:09 +0100

    Now returning information as JSON (#2)

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,15 @@ FROM node:21-alpine
 
 WORKDIR /app
 
-COPY index.js /app
+COPY package.json index.js /app/
+
+RUN npm set progress=false && \
+  npm config set depth 0 && \
+  npm install --omit=dev && \
+  npm audit fix
 
 ENV PORT 8080
 
 EXPOSE $PORT
 
-CMD ["node", "index.js"]
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const http = require('http');
+const http = require('node:http');
+const requestIp = require('request-ip');
+
 const port = process.env.PORT;
 const server = http.createServer(function (req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = requestIp.getClientIp(req);
   let date = new Date();
   console.log(date.toISOString(), ip, req.method, req.url);
 
-  res.end("OK\n");
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    timestamp: date.toISOString(),
+    clientIp: ip,
+    method: req.method,
+    url: req.url
+  }));
 });
 
 server.listen(port);
